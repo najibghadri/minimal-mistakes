d38283ac4f7b664e0438e709e5ee847aee881f54
najibghadri <najibg96@gmail.com>
2020-06-01 04:30:48 +0200

    ok

---
+++ precache-manifest.a9d468adf70545d3fb001142745cd1e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed67e5a9472edadcad75c3ae5908fcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd17ddc2b80ab3f372ae",
+    "url": "/static/css/main.83e769a2.chunk.css"
+  },
+  {
+    "revision": "040aa46daf514ca05775",
+    "url": "/static/js/2.8251f010.chunk.js"
+  },
+  {
+    "revision": "e256f392c3bdbde6bee9ac6b81842036",
+    "url": "/static/js/2.8251f010.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd17ddc2b80ab3f372ae",
+    "url": "/static/js/main.38e546a6.chunk.js"
+  },
+  {
+    "revision": "5b0add79504e44f27c39",
+    "url": "/static/js/runtime-main.e2a17ba8.js"
+  }
+]);
